07fd8a1fbea60d74a5e002535e17ba39ffb70dec
Tashley <tashley@ennov.io>
2019-06-01 22:12:31 +0400

    Remove dead codes

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IndexComponent } from './index/index.component';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
