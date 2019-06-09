81716df72c41cce356b3b92f5469cc62405c4f0f
Tashley <tashley@ennov.io>
2019-06-09 12:41:50 +0400

    small deploy fix

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname + '/dist'));
-
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/my-dream-app'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/my-dream-app/index.html'
+  res.sendFile(path.join(__dirname + '/dist/my-dream-app/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-// PathLocationStrategy
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'))
-})
