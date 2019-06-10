8f573afb40f6f83e07f09fc0b917a08cf9af9b10
Tashley <tashley@ennov.io>
2019-06-10 19:43:05 +0400

    small fix

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ThemeService } from './../theme.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  public changeEvent(value):void {
-      if (value.checked == true ) {
-        ThemeService.toggleDark()
-      }
-      else {
-        ThemeService.toggleLight()
-      }
-
-    }
   
+  constructor(private themeService: ThemeService) { }
+  ngOnInit() {
+  }
 
-    ngOnInit() {
+  public changeEvent(value): void {
+
+    console.log('args', arguments);
+
+    if (value.checked == true) {
+      //ThemeService.toggleDark();
+      this.themeService.toggleDark();
+    }
+    else {
+      //ThemeService.toggleLight();
+      this.themeService.toggleLight();
+    }
+
   }
 
 }
