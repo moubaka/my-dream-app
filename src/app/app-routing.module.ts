26945a27befa0d6b8efddf203458c890b5e715f0
Annaelle <annagenev@gmail.com>
2019-06-10 19:41:27 +0400

    Auto stash before merge of "master" and "origin/master"
    Landing page setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent }  from "./homepage/homepage.component";
 import { RegisterComponent } from './register/register.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 const routes: Routes = [
   //{path: 'homepage', component: HomepageComponent},
   {path: 'homepage', component: HomepageComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'landingpage', component: LandingpageComponent},
   { path: '', redirectTo: '/homepage', pathMatch: 'full'}
 ];
 
